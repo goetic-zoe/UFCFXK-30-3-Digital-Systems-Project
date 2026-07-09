f1eb9b36946c5147486d2a67467c49581d693c36
goetic-zoe <github@bushycargo.com>
2026-07-09 05:26:04 +0100

    Added image upload and display functionality.

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ import tensorflow as tf
 import numpy as np
 from PIL import Image
 import io
+import os
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'static/uploads'
 
 # Load your trained model (make sure the path is correct)
 model = tf.keras.models.load_model('models/trained_cancer_cnn.keras')
@@ -12,7 +14,7 @@ model = tf.keras.models.load_model('models/trained_cancer_cnn.keras')
 
 @app.route('/')
 def dashboard():
-    return render_template('index.html', prediction=None)
+    return render_template('index.html', prediction=None, image_filename='placeholder.jpg')
 
 
 @app.route('/predict', methods=['POST'])
@@ -24,20 +26,27 @@ def predict():
     if file.filename == '':
         return redirect(request.url)
 
-    # Process the image
-    img = Image.open(io.BytesIO(file.read())).resize((180, 180))
-    img_array = np.array(img) / 255.0
-    img_array = np.expand_dims(img_array, axis=0)
+    # Save the uploaded image
+    image_filename = file.filename
+    file.save(str(os.path.join(app.config['UPLOAD_FOLDER'], str(image_filename))))
+
+    image = tf.io.read_file(os.path.join(app.config['UPLOAD_FOLDER'], str(image_filename)))
+    image = tf.image.decode_image(image, channels=3, expand_animations=False)
+    image_size = (180,180)
+    image = tf.image.resize(image, image_size)
+    image = tf.cast(image, tf.float32) / 255.0
+    image_arr = tf.keras.utils.img_to_array(image)
+    image_arr = np.expand_dims(image_arr, axis=0)
 
     # Make prediction
-    prediction = model.predict(img_array)
+    prediction = model.predict(image_arr)
     predicted_class_index = np.argmax(prediction[0])
     class_names = ['actinic keratosis', 'basal cell carcinoma', 'dermatofibroma', 'melanoma',
                    'nevus', 'pigmented benign keratosis', 'seborrheic keratosis',
                    'squamous cell carcinoma', 'vascular lesion']
     predicted_class = class_names[predicted_class_index]
 
-    return render_template('index.html', prediction=predicted_class)
+    return render_template('index.html', prediction=predicted_class, image_filename=image_filename)
 
 
 if __name__ == '__main__':
